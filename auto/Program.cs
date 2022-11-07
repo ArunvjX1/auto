5639ed882d92398a66aaf2654ce299039ed92a45
ARUN VIJAY <ARUN VIJAY@ARUNVJX1>
2022-11-07 00:15:50 +0000

    Add project files.

---
+++ auto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using auto.com.auto;
+namespace auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kola te = new kola("hello Arun Vijay");
+            Console.WriteLine(te);
+            overload tum = new overload();
+            Console.WriteLine("\n",tum,"\n");
+            tum.over();
+
+            tum.over(16995);
+            Console.ReadLine();
+
+            
+           
+        }
+    }
+}
---
+++ auto/com/auto/auto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto.com.auto
+{
+    class kola
+    {
+        public kola()
+        {
+
+            Console.WriteLine("this is the default Value");
+        }
+        public kola(int a)
+        {
+
+            Console.WriteLine("this is the default Value of int {0}", a);
+        }
+        public kola(string test)
+        {
+            Console.WriteLine("This is the Value of the string: {0}", test);
+        }
+    }
+    public class overload
+        {
+
+           public void over() {
+                Console.WriteLine("this is the default overloading method we are creating!");
+            }
+         public void over(int a) {
+                Console.WriteLine(a);
+            
+            }
+        public void over(string check) {
+                Console.WriteLine(check);
+            }
+        }
+    
+    
+    
+
+    internal class auto
+    {
+
+    }
+}
